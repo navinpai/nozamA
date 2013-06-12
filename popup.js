ab4570f8c978f66e12e70c0d706c8d6c33cb9789
Navin Pai <lifeofnavin@gmail.com>
2013-06-13 01:23:58 +0530

    Removed alert and added DOM element instead

--- popup.js
+++ popup.js
@@ -3,12 +3,16 @@ findCompetitor();
 function findOnAmazon(title){
 amazonSearchUrl='http://www.amazon.in/s/ref=nb_sb_noss_1/278-4309243-0016545?url=search-alias%3Daps&field-keywords='+encodeURIComponent(title);
   $.get(amazonSearchUrl,function(data) {
-    if($("#noResultsTitle",data).get(0))
-        alert("Amazon doesn't have this... yet!");
+    if($("#noResultsTitle",data).get(0)){
+        message="Amazon doesn't have this... yet!"
+        addToPage(message)
+        }
     else{
         result=$("#result_0 > .rsltL > .newp",data).find('a').find('span.bld.lrg.red').html();
-        alert("Available on Amazon for Rs.:" +result.substr(result.lastIndexOf(">")+1));
-    }
+        message="Available on <span id=\"amazonlogo\">&nbsp;</span> for <span style=\"font-weight: bold\">Rs." +result.substr(result.lastIndexOf(">")+1)+"</span> (<a href=\""+amazonSearchUrl+"\">link</a>)"
+        addToPage(message)
+ 
+        }
     });
 
 }
@@ -22,4 +26,11 @@ function findCompetitor(){
         findOnAmazon(title);
       }
  
+}
+
+function addToPage(message){
+$(function(){
+    $("#fk-mainhead-id").prepend("<p class=\"flipkart-price\">"+message+"</p>");
+    $(".flipkart-price").show()
+});
 }
