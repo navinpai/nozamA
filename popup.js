6b1d17346c78a8b07df8f8f653611b0b9b49bab7
Navin Pai <lifeofnavin@gmail.com>
2013-06-14 21:35:52 +0530

    Added test alert

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ findCompetitor();
 function findOnAmazon(title){
 amazonSearchUrl='http://www.amazon.in/s/ref=nb_sb_noss_1/278-4309243-0016545?url=search-alias%3Daps&field-keywords='+encodeURIComponent(title);
   $.get(amazonSearchUrl,function(data) {
+      alert(data)
     if($("#noResultsTitle",data).get(0)){
         message="Amazon doesn't have this... yet!"
         addToPage(message)
@@ -29,6 +30,8 @@ function findCompetitor(){
 }
 
 function addToPage(message){
+        alert(message)
+
 $(function(){
     $("#fk-mainhead-id").prepend("<p class=\"flipkart-price\">"+message+"</p>");
     $(".flipkart-price").show()
